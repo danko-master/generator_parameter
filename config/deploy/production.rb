603d54680b4ad28cd84383d416cccb184a87a680
danko-progress <danko-progress@progress-hp.(none)>
2014-05-20 16:18:20 +0400

    edit cap3

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -44,3 +44,48 @@ set :rails_env,    "production"
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+namespace :unicorn do
+  pid_path = "#{deploy_to}/shared/pids"
+  unicorn_pid = "#{pid_path}/unicorn.pid"
+
+  def run_unicorn
+    execute "#{fetch(:bundle_binstubs)}/unicorn", "-c #{release_path}/config/unicorn.rb -D -E #{fetch(:stage)}"
+  end
+
+  desc 'Start unicorn'
+  task :start do
+    on roles(:app) do
+      run_unicorn
+    end
+  end
+
+  desc 'Stop unicorn'
+  task :stop do
+    on roles(:app) do
+      if test "[ -f #{unicorn_pid} ]"
+        execute :kill, "-QUIT `cat #{unicorn_pid}`"
+      end
+    end
+  end
+
+  desc 'Force stop unicorn (kill -9)'
+  task :force_stop do
+    on roles(:app) do
+      if test "[ -f #{unicorn_pid} ]"
+        execute :kill, "-9 `cat #{unicorn_pid}`"
+        execute :rm, unicorn_pid
+      end
+    end
+  end
+
+  desc 'Restart unicorn'
+  task :restart do
+    on roles(:app) do
+      if test "[ -f #{unicorn_pid} ]"
+        execute :kill, "-USR2 `cat #{unicorn_pid}`"
+      else
+        run_unicorn
+      end
+    end
+  end
+end
